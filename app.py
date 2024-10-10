bd08ea857a4cfbd6263d4b6ef3b6215165082995
Hans Steffens <hans@steffens.co>
2024-10-09 20:41:17 -0500

    Create app.py file and import data

---
+++ app.py
@@ -0,0 +1,7 @@
+# Import the constants file that contains teams and players data
+from constants import PLAYERS, TEAMS
+
+if __name__ == "__main__":
+    # Test if import works by printing TEAMS and PLAYERS
+    print("Teams:", TEAMS)
+    print("Players:", PLAYERS)
